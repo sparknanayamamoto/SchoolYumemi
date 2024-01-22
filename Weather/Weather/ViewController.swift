e457b1500effe73790d032961f28269bf80116ef
spark-04 <>
2024-01-22 15:42:06 +0900

    ThreadBlock途中

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -15,12 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var minTemperatureLabel: UILabel!
     @IBOutlet weak var maxTemperatureLabel: UILabel!
+    @IBOutlet weak var startActivityIndicator: UIActivityIndicatorView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         yumemiTenki.delegate = self
         NotificationCenter.default.addObserver(
             self,
@@ -28,7 +30,7 @@ class ViewController: UIViewController {
             name: UIApplication.willEnterForegroundNotification,
             object: nil)
         
-        self.updateWeather()
+        startActivityIndicator.hidesWhenStopped = true
     }
     
     
@@ -43,57 +45,68 @@ class ViewController: UIViewController {
     }
     
     
+    
     @IBAction func closeButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     
     @IBAction func whetherChange(_ sender: Any) {
+        self.startActivityIndicator.startAnimating()
         yumemiTenki.setYumemiWeather()
     }
-    
 }
+
 
 
 extension ViewController:YumemiDelegate {
+    
     func setMinTemperature(min: Int) {
-        self.minTemperatureLabel.text = String(min)
+        DispatchQueue.main.async {
+            self.minTemperatureLabel.text = String(min)
+        }
     }
     
     func setMaxTemperature(max: Int) {
-        self.maxTemperatureLabel.text = String(max)
+        DispatchQueue.main.async {
+            self.maxTemperatureLabel.text = String(max)
+        }
     }
-    
     
     func setErrorWeather(alertMessage: String) {
-        let dialog = UIAlertController(title: "確認", message: alertMessage, preferredStyle: .alert)
-        dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(dialog, animated: true, completion: nil)
+        DispatchQueue.main.async {
+            let dialog = UIAlertController(title: "確認", message: alertMessage, preferredStyle: .alert)
+            dialog.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(dialog, animated: true, completion: nil)
+            self.startActivityIndicator.stopAnimating()
+        }
     }
     
-    
     func setWeatherImage(type: String) {
-        
-        var imageName = "sunny"
-        var tintColor = UIColor.red
-        
-        switch type {
-        case "sunny":
-            imageName = "sunny"
-            tintColor = UIColor.red
-        case "cloudy":
-            imageName = "cloudy"
-            tintColor = UIColor.gray
-        case "rainy":
-            imageName = "rainy"
-            tintColor = UIColor.blue
-        default:
-            break
+            var imageName = "sunny"
+            var tintColor = UIColor.red
+            
+            switch type {
+            case "sunny":
+                imageName = "sunny"
+                tintColor = UIColor.red
+            case "cloudy":
+                imageName = "cloudy"
+                tintColor = UIColor.gray
+            case "rainy":
+                imageName = "rainy"
+                tintColor = UIColor.blue
+            default:
+                break
+            }
+            
+        DispatchQueue.main.async { [self] in
+            weatherIcon.image = UIImage(named: imageName)
+            weatherIcon.tintColor = tintColor
+            startActivityIndicator.stopAnimating()
         }
-        
-        weatherIcon.image = UIImage(named: imageName)
-        weatherIcon.tintColor = tintColor
     }
     
 }
+
 
--- Weather/WeatherControl.swift
+++ Weather/WeatherControl.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import YumemiWeather
+import Dispatch
 
 
 struct WeatherArea: Codable {
@@ -24,48 +25,54 @@ protocol YumemiDelegate {
     
 }
 
+
 // 処理を任されるクラス
 class YumemiTenki {
     var delegate: YumemiDelegate?
     
     func setYumemiWeather() {
         
+        DispatchQueue.global().async {
+        
         let requestJson = WeatherArea(area: "tokyo", date: "2020-04-01T12:00:00+09:00")
         
-        do {
-            let encoder = JSONEncoder()
-            let yumemiJsonDate = try encoder.encode(requestJson)
-            
-            guard let jsonData = String(data: yumemiJsonDate, encoding: .utf8)
-            else {
-                return
+            do {
+                let encoder = JSONEncoder()
+                let yumemiJsonDate = try encoder.encode(requestJson)
+                
+                guard let jsonData = String(data: yumemiJsonDate, encoding: .utf8)
+                else {
+                    return
+                }
+                
+                let weatherCondition = try YumemiWeather.syncFetchWeather(jsonData)
+                
+                guard let jsonData =  weatherCondition.data(using: .utf8),
+                      let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                      let minTemperature = json["min_temperature"] as? Int,
+                      let maxTemperature = json["max_temperature"] as? Int,
+                      let weatherCondition = json["weather_condition"] as? String
+                        
+                else {
+                    return
+                }
+                
+                self.delegate?.setWeatherImage(type: weatherCondition)
+                self.delegate?.setMinTemperature(min: minTemperature)
+                self.delegate?.setMaxTemperature(max: maxTemperature)
+                
+            } catch YumemiWeatherError.unknownError {
+                self.delegate?.setErrorWeather(alertMessage: "不明なエラーが発生しました")
+                
+            } catch let error {
+                print(error)
             }
-            
-            let weatherCondition = try YumemiWeather.fetchWeather(jsonData)
-            
-            guard let jsonData =  weatherCondition.data(using: .utf8),
-                  let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
-                  let minTemperature = json["min_temperature"] as? Int,
-                  let maxTemperature = json["max_temperature"] as? Int,
-                  let weatherCondition = json["weather_condition"] as? String
-                    
-            else {
-                return
-            }
-            
-            self.delegate?.setWeatherImage(type: weatherCondition)
-            self.delegate?.setMinTemperature(min: minTemperature)
-            self.delegate?.setMaxTemperature(max: maxTemperature)
-            
-        } catch YumemiWeatherError.unknownError {
-            self.delegate?.setErrorWeather(alertMessage: "不明なエラーが発生しました")
-            
-        } catch let error {
-            print(error)
         }
     }
     
 }
+
+
 
 
 
